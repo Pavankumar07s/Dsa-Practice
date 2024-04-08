3f2274e535f7d409e9500f931d0e409ddecf5f45
Pavankumar07s <pawankumar1462693@gmail.com>
2024-04-08 11:47:15 +0530

    fixed logic

--- slidingWindow/07Longest Substring With Without Repeating Characters.js
+++ slidingWindow/07Longest Substring With Without Repeating Characters.js
@@ -1,58 +1,47 @@
-const s = "pwwkew"
-const strToMap=(j,map)=>{
-    if(map.has(j)){
-        map.set(j,map.get(j)+1)
-    }
-    else{
-        map.set(j,1)
-    }
+const s = "abcabcbb";
 
-}
-const stringTomapRed=(i,map)=>{
-    if(map.has(i)){
-        map.set(i,map.get(i)-1)
-        if(map.get(i)==0){
-            map.delete(i)
+const strToMap = (j, map) => {
+    if (map.has(j)) {
+        map.set(j, map.get(j) + 1);
+    } else {
+        map.set(j, 1);
+    }
+};
+
+const stringTomapRed = (i, map) => {
+    if (map.has(i)) {
+        map.set(i, map.get(i) - 1);
+        if (map.get(i) === 0) {
+            map.delete(i);
         }
     }
+};
 
-    
-}
-const LongestSubStr=(s)=>{
-    let i=0
-    let j=0
-    let lengthSUb=0
-    let map =new Map()
-    while(j<s.length){
-        //calculation for j
-        strToMap(s[j],map)
+const LongestSubStr = (s) => {
+    let i = 0;
+    let j = 0;
+    let maxLength = 0;
+    let map = new Map();
 
-        if(map.get(s[j])==1){
-            j++
+    while (j < s.length) {
+        const currentChar = s[j];
+        if (map.has(currentChar)) {
+            // If the character is already in the map, move the left pointer
+            // to the position right after the last occurrence of the character.
+            i = Math.max(i, map.get(currentChar) + 1);
         }
 
-        else if(map.get(s[j])==2){
-            let temp=j-i+1
-            lengthSUb=Math.max(temp,lengthSUb)
-            j++
-        }
+        // Update the maximum length
+        maxLength = Math.max(maxLength, j - i + 1);
 
-        else if(map.get(s[j])>1){
-            while(map.get(s[j])<=1){
-                stringTomapRed=(i,map)
-                i++
-            }
-            if(map.get(s[j])==1){
-                let temp=j-i+1
-                lengthSUb=Math.max(temp,lengthSUb)
-            }
-            j++
-        }
+        // Update the position of the current character in the map
+        map.set(currentChar, j);
 
+        // Move right pointer to the next character
+        j++;
     }
-    return lengthSUb
-}
 
-console.log(LongestSubStr(s))
+    return maxLength;
+};
 
-//code work properly
+console.log(LongestSubStr(s));
