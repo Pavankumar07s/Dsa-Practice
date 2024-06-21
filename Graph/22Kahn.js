8e75a1b64a4a433e8415cc44269eee80bd59e765
Pavankumar07s <pawankumar14662693@gmail.com>
2024-06-21 11:32:48 +0530

    21 question edited and 22 done

--- Graph/22Kahn.js
+++ Graph/22Kahn.js
@@ -1,11 +1,11 @@
 // Given an adjacency list for a Directed Acyclic Graph (DAG) where adj_list[i] contains a list of all vertices j such that there is a directed edge from vertex i to vertex j, with  V  vertices and E  edges, your task is to find any valid topological sorting of the graph.
 // In a topological sort, for every directed edge u -> v,  u must come before v in the ordering.
-
 class Graph {
   constructor(v) {
     this.graph = new Map(); // Implementing graph using Map
     this.visited = Array(v + 1).fill(false);
     this.Stack = [];
+    this.topologicalOrder = [];
   }
 
   // Utility function to add edge in an directed graph
@@ -19,17 +19,35 @@ class Graph {
     }
   }
 
-  Dfs(src) {
-    this.visited[src] = 1;
-    for (let neighbor of this.graph.get(src) || []) {
-      if (!this.visited[neighbor]) {
-        if (this.Dfs(neighbor)) {
-          return true;
+  Bfs(v) {
+    let inDegree = Array(v).fill(0);
+
+    // Compute in-degree of each node
+    for (let [currentNode, neighbors] of this.graph.entries()) {
+      for (let neighbor of neighbors) {
+        inDegree[neighbor]++;
+      }
+    }
+    let queue = [];
+    for (let i = 0; i < v; i++) {
+      if (inDegree[i] === 0) {
+        queue.push(i);
+      }
+    }
+
+    while (queue.length) {
+      let currentNode = queue.shift();
+      this.topologicalOrder.push(currentNode);
+
+      if (this.graph.has(currentNode)) {
+        for (let neighbor of this.graph.get(currentNode)) {
+          inDegree[neighbor]--;
+          if (inDegree[neighbor] === 0) {
+            queue.push(neighbor);
+          }
         }
       }
     }
-    this.Stack.unshift(src);
-    return false;
   }
 }
 
@@ -44,16 +62,8 @@ function main() {
   g.addEdge(4, 1);
   g.addEdge(4, 0);
 
-  for (let i = 0; i < v; i++) {
-    if (!g.visited[i]) {
-      g.Dfs(i);
-    }
-  }
+  g.Bfs(v);
 
-  console.log(g.Stack);
-  console.log(`this is the Topological sort `);
-  for (let elem of g.Stack) {
-    console.log(`${elem}`);
-  }
+  console.log(g.topologicalOrder);
 }
 main();
