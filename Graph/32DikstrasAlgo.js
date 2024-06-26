8f4ac971ed057908ae546120b421964e99b16963
Pavankumar07s <pawankumar14662693@gmail.com>
2024-06-26 22:16:27 +0530

    just done with smile

--- Graph/32DikstrasAlgo.js
+++ Graph/32DikstrasAlgo.js
@@ -1,3 +1,5 @@
+// Given a weighted, undirected and connected graph of V vertices and an adjacency list adj where adj[i] is a list of lists containing two integers where the first integer of each list j denotes there is edge between i and j , second integers corresponds to the weight of that  edge . You are given the source vertex S and You to Find the shortest distance of all the vertex's from the source vertex S. You have to return a list of integers denoting shortest distance between each node and Source vertex S.
+
 const { Heap } = require('heap-js');
 
 // Min Heap by default
