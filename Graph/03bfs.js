ca1c146e85b185805c468425647b1f1fbc61193c
Pavankumar07s <pawankumar14662693@gmail.com>
2024-07-13 23:31:21 +0530

    little refactor

--- Graph/03bfs.js
+++ Graph/03bfs.js
@@ -1,41 +1,40 @@
 // Class to represent a graph using adjacency list
 class Graph {
-    constructor() {
-        this.adjList = {};
-    }
+  constructor() {
+    this.adjList = {};
+  }
 
-    // Function to add an edge to the graph
-    addEdge(u, v) {
-        if (!this.adjList[u]) this.adjList[u] = [];
-        this.adjList[u].push(v);
-    }
+  // Function to add an edge to the graph
+  addEdge(u, v) {
+    if (!this.adjList[u]) this.adjList[u] = [];
+    this.adjList[u].push(v);
+  }
 
-    // Function to perform Breadth First Search on a graph represented using adjacency list
-    bfs(startNode) {
-        // Create a queue for BFS
-        const queue = [];
-        const visited = new Array(Object.keys(this.adjList).length).fill(false);
+  // Function to perform Breadth First Search on a graph represented using adjacency list
+  bfs(startNode) {
+    // Create a queue for BFS
+    const queue = [];
+    const visited = new Array(Object.keys(this.adjList).length).fill(false);
 
-        // Mark the current node as visited and enqueue it
-        visited[startNode] = true;
-        queue.push(startNode);
-        // Iterate over the queue
-        while (queue.length !== 0) {
-            // Dequeue a vertex from queue and print it
-            const currentNode = queue.shift();
-            console.log(currentNode + " ");
-            
-            // Get all adjacent vertices of the dequeued vertex currentNode
-            // If an adjacent has not been visited, then mark it visited and enqueue it
-            for (const neighbor of this.adjList[currentNode] || []) {
-                if (!visited[neighbor]) {
-                    visited[neighbor] = true;
-                    queue.push(neighbor);
-                }
-            }
-            
+    // Mark the current node as visited and enqueue it
+    visited[startNode] = true;
+    queue.push(startNode);
+    // Iterate over the queue
+    while (queue.length !== 0) {
+      // Dequeue a vertex from queue and print it
+      const currentNode = queue.shift();
+      console.log(currentNode + " ");
+
+      // Get all adjacent vertices of the dequeued vertex currentNode
+      // If an adjacent has not been visited, then mark it visited and enqueue it
+      for (const neighbor of this.adjList[currentNode] || []) {
+        if (!visited[neighbor]) {
+          visited[neighbor] = true;
+          queue.push(neighbor);
         }
+      }
     }
+  }
 }
 
 // Create a graph
